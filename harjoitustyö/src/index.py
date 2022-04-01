a24b59bc42e1725c41104007054603db3811d482
ronituohino <tuohinoroni@gmail.com>
2022-04-01 15:47:02 +0300

    Add: Pygame testing

--- harjoitustyö/src/index.py
+++ harjoitustyö/src/index.py
@@ -1,1 +1,21 @@
-print("start!")
+import pygame
+
+pygame.init()
+
+# Set up the drawing window
+screen = pygame.display.set_mode([500, 500])
+
+running = True
+while running:
+    # Window close button event
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((255, 255, 255))
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    # Flip the display
+    pygame.display.flip()
+
+pygame.quit()
